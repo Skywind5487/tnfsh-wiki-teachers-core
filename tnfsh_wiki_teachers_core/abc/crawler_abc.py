1c6a96fb385df5060db9226ad0df3f2eecc41ee9
Skywind5487 <skywind5487@gmail.com>
2025-05-31 12:39:33 +0800

    init and cache, index, rev_index

--- tnfsh_wiki_teachers_core/abc/crawler_abc.py
+++ tnfsh_wiki_teachers_core/abc/crawler_abc.py
@@ -8,22 +8,25 @@ class BaseCrawlerABC(ABC, Generic[TResult]):
     Crawler 層的抽象基底類，規範所有爬蟲/資料抓取器的標準介面。
     """
     @abstractmethod
-    async def fetch_raw(self, *args, **kwargs) -> Any:
+    async def fetch_raw(self, *args:Any, **kwargs:Any) -> Any:
         """
         從其他地方拿到資料
         """
         pass
 
     @abstractmethod
-    def parse(self, raw: Any, *args, **kwargs) -> TResult:
+    def parse(self, raw: Any, *args:Any, **kwargs:Any) -> TResult:
         """
         解析資料成好的資料結構
         """
         pass
     
-    @abstractmethod
-    async def fetch(self, *args, **kwargs) -> TResult:
+    @classmethod
+    async def fetch(cls, *args:Any, **kwargs:Any) -> TResult:
         """
         拿資料並解析
         """
-        pass
+        self = cls(*args, **kwargs)
+        raw = await self.fetch_raw()
+        result = self.parse(raw)
+        return result
